983ee4efb2dd80f89c26d33a729bc949fe84df37
Kirillbiliashov <Kirillbeliashov14@gmail.com>
2023-05-31 14:27:05 +0300

    Db entities as data classes were added

---
+++ app/src/main/java/com/example/moneytrackerapp/data/entity/Category.kt
@@ -0,0 +1,10 @@
+package com.example.moneytrackerapp.data.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "category")
+data class Category(
+    @PrimaryKey val id: Int,
+    val name: String
+)
---
+++ app/src/main/java/com/example/moneytrackerapp/data/entity/Expense.kt
@@ -0,0 +1,21 @@
+package com.example.moneytrackerapp.data.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "expense",
+    foreignKeys = [ForeignKey(
+        entity = Category::class, parentColumns = ["id"],
+        childColumns = ["category_id"]
+    )]
+)
+data class Expense(
+    @PrimaryKey val id: Long,
+    val name: String,
+    val sum: Double,
+    val date: Long,
+    @ColumnInfo(name = "category_id") val categoryId: Int
+)
